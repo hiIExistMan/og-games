b6b13138e086e27992cde25d79da7e39fbbbd550
hiIExistMan <codymotes2010@gmail.com>
2021-03-01 18:28:40 -0500

    adding buttons to go to games

--- load.js
+++ load.js
@@ -5,8 +5,11 @@ for(let game of games) {
   let g = document.createElement("div");
   // add image and title from images folder and the "game" variable
   g.innerHTML =
-  `${game}<br/>
-  <img width="100" height="150" src="/images/${game}.png"/>`;
+  `
+  <button onclick="window.location.replace('/games/${game}')">
+  ${game}<br/>
+  <img width="100" height="150" src="/images/${game}.png"/>
+  </button>`;
   //append game to list
   console.log(document.querySelector("#games"));
   document.querySelector("#games").appendChild(g);
